c3118358989b3ff889232460612f90b567fcf4cc
Ad-Forge <corentin.monmasson@gmail.com>
2016-06-02 10:03:53 +0200

    First Commit

---
+++ assets/js/anims.js
@@ -0,0 +1,48 @@
+function menuDeploy(event){
+  if(!deployed){
+    deployed=true;
+    header.className = "deployed";
+    nav.className = "deployed";
+    bottomM.className = "deployed";
+    boxMenu.firstChild.firstChild.className = "closeMenu";
+  } else{
+    deployed = false;
+    header.className = "";
+    nav.className = "";
+    bottomM.className = "";
+    boxMenu.firstChild.firstChild.className = "";
+  }
+}
+// function closeMenu(){
+//   if(deployed){
+//     deployed = false;
+//     header.className = "";
+// }
+var deployed = false;
+var boxMenu = document.getElementById("box-menu");
+var bottomM = document.getElementById("bottomMenu");
+var header = document.getElementsByTagName("header")[0];
+var nav = document.getElementsByTagName("nav")[0];
+var links = document.getElementById("links");
+//var body = document.getElementsByTagName("body")[0];
+
+boxMenu.firstChild.addEventListener("click",menuDeploy,false);
+links.firstChild.addEventListener("click",menuDeploy,false);
+//body.addEventListener("click",closeMenu,false);
+
+//////////
+//HOVERS//
+//////////
+$(".thumLinks").mouseenter(function(event){
+  console.log("enter thumb :");
+  $(".thumLinks").not(this).parent().toggleClass("nonhover","true");
+});
+$(".thumLinks").mouseleave(function(event){
+  console.log("leave thumb :");
+  $(".thumLinks").not(this).parent().toggleClass("nonhover","false");
+});
+
+// $("#box-menu div").hover(function(event){
+//   console.log("hover menubox :");
+//   $("#box-menu div").toggleClass("hover");
+// });
